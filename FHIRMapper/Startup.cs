38819a8e917e1b486839e1448d5720bb1c82a166
dishsebs <35844535+dishsebs@users.noreply.github.com>
2020-07-10 11:40:39 -0400

    update Angular version from 5.2 to 7.2

--- FHIRMapper/Startup.cs
+++ FHIRMapper/Startup.cs
@@ -63,6 +63,7 @@ namespace FHIRMapper
                 if (env.IsDevelopment())
                 {
                     spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
